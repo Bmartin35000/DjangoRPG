3e5bbe779064a868722ec5425316c896ce858a4d
baptiste.martin <batiste35170@laposte.net>
2025-03-31 14:23:53 +0200

    finishing mvc for character

--- src/character/urls.py
+++ src/character/urls.py
@@ -19,4 +19,5 @@ from .views import CharacterView
 
 urlpatterns = [
     path('', CharacterView.as_view()),
+    path('<int:id>', CharacterView.as_view()),
 ]
--- src/character/views.py
+++ src/character/views.py
@@ -1,25 +1,46 @@
 import json
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from character.models import Character
 from character.serializer import CharacterSerializer
 
-# Create your views here.
-
 class CharacterView(TemplateView):
     def get(self, req):
-        characters = Character.objects.all()
-        print(characters)
-        return JsonResponse("ok", safe=False)
+        characters = Character.objects.all() # retrieve queryset
+        # serializing (turn an object into a json)
+        characterSerializer = CharacterSerializer(characters, many=True)
+        return JsonResponse(characterSerializer.data, safe=False)
     
     def post(self, req: HttpRequest):
+        # deserializing (turn json into an object)
         bodyDict = json.loads(req.body) # json to dict
         characterSerializer = CharacterSerializer(data=bodyDict) # dict to modelSerializer
         if characterSerializer.is_valid():
             characterSerializer.save()
-            return JsonResponse("ok", safe=False)
+            return HttpResponse(status=201)
+        else:
+            return JsonResponse(characterSerializer.errors, status=400)
+        
+    def put(self, req: HttpRequest):
+        bodyDict = json.loads(req.body)
+        charInDb = get_object_or_404(Character, pk=bodyDict["id"])
+        # updating the charInDB with the bodyDict
+        characterSerializer = CharacterSerializer(charInDb, data=bodyDict)
+        if characterSerializer.is_valid():
+            characterSerializer.save()
+            return HttpResponse(status=201)
         else:
             print(characterSerializer.errors)
-            return JsonResponse("ko", safe=False, status=400)
-            
+            return JsonResponse(characterSerializer.errors, status=400)
+        
+    def delete(self, req: HttpRequest, id):
+        myCharacter = get_object_or_404(Character, pk=id)
+        myCharacter.delete()
+        return HttpResponse()
+    
+    def delete(self, req: HttpRequest):
+        myCharacters = Character.objects.all()
+        myCharacters.delete()
+        return HttpResponse()
